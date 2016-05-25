42903c82c7e4e730bc7fe6ddbce450d22ba2c140
Joakim Sandström <joakim.s@webbkontakt.net>
2016-05-25 23:10:44 +0200

    Update install_x11.sh

--- install_x11.sh
+++ install_x11.sh
@@ -2,12 +2,12 @@
 sudo apt-get update 2>&1 | dialog --title "Updating package database..." --infobox "\nPlease wait...\n" 11 70
 sudo apt-get --show-progress -y install lxde-core xserver-xorg xinit  2>&1 | grep --line-buffered -oP "(\d+(\.\d+)?(?=%))" | dialog --title "Installing X11 and LXDE-core" --gauge "\nPlease wait...\n" 11 70
 dialog --title "Download and install" --infobox "\nDownloading chromium and installing\n"  11 70
-wget http://ftp.us.debian.org/debian/pool/main/libg/libgcrypt11/libgcrypt11_1.5.0-5+deb7u4_armhf.deb
-wget http://launchpadlibrarian.net/218525709/chromium-browser_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
-wget http://launchpadlibrarian.net/218525711/chromium-codecs-ffmpeg-extra_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
-sudo dpkg -i libgcrypt11_1.5.0-5+deb7u4_armhf.deb
-sudo dpkg -i chromium-codecs-ffmpeg-extra_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
-sudo dpkg -i chromium-browser_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
+wget -q http://ftp.us.debian.org/debian/pool/main/libg/libgcrypt11/libgcrypt11_1.5.0-5+deb7u4_armhf.deb
+wget -q http://launchpadlibrarian.net/218525709/chromium-browser_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
+wget -q http://launchpadlibrarian.net/218525711/chromium-codecs-ffmpeg-extra_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
+sudo dpkg -q -i libgcrypt11_1.5.0-5+deb7u4_armhf.deb
+sudo dpkg -q -i chromium-codecs-ffmpeg-extra_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
+sudo dpkg -q -i chromium-browser_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
 dialog --title "Download and install" --infobox "\nDownloading launcher and installing\n"  11 70
 wget https://github.com/zjoasan/x11-osmc/raw/master/install_x11.zip
 unzip -q -o install_x11.zip
