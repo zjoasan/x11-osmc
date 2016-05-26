f63a78e52689c6a04eba2f0502652f353f490373
Joakim Sandström <joakim.s@webbkontakt.net>
2016-05-26 21:08:34 +0200

    Update install_x11.sh

--- install_x11.sh
+++ install_x11.sh
@@ -3,6 +3,7 @@ sudo apt-get update 2>&1 | dialog --title "Updating package database..." --infob
 dialog --title "Trying install x11" --infobox "\nPlease wait...\n" 11 70
 sudo apt-get -q install lxde-core xserver-xorg xinit  2>&1 
 if [ $? -gt 0 ]; then
+dialog --title "Installing dependencies..." --infobox "\nPlease wait...\n" 11 70
 apt-get -f -q --force-yes --yes install >/dev/null 2>&1
 fi
 sudo apt-get --show-progress -y install lxde-core xserver-xorg xinit  2>&1 | grep --line-buffered -oP "(\d+(\.\d+)?(?=%))" | dialog --title "Installing X11 and LXDE-core" --gauge "\nPlease wait...\n" 11 70
@@ -10,13 +11,13 @@ dialog --title "Download and install" --infobox "\nDownloading chromium and inst
 wget -q http://ftp.us.debian.org/debian/pool/main/libg/libgcrypt11/libgcrypt11_1.5.0-5+deb7u4_armhf.deb
 wget -q http://launchpadlibrarian.net/218525709/chromium-browser_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
 wget -q http://launchpadlibrarian.net/218525711/chromium-codecs-ffmpeg-extra_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
-sudo dpkg -i libgcrypt11_1.5.0-5+deb7u4_armhf.deb
-sudo dpkg -i chromium-codecs-ffmpeg-extra_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
-sudo dpkg -i chromium-browser_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
+sudo dpkg -q -i libgcrypt11_1.5.0-5+deb7u4_armhf.deb
+sudo dpkg -q -i chromium-codecs-ffmpeg-extra_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
+sudo dpkg -q -i chromium-browser_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
 if [ $? -gt 0 ]; then
 apt-get -f --force-yes --yes install >/dev/null 2>&1
 fi
-sudo dpkg -i chromium-browser_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
+sudo dpkg -q -i chromium-browser_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
 dialog --title "Download and install" --infobox "\nDownloading launcher and installing\n"  11 70
 wget -q https://github.com/zjoasan/x11-osmc/raw/master/install_x11.zip
 unzip -q -o install_x11.zip
