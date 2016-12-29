e7c5d2b66e6aff5d72c08f6e9b1f2084a7c379fa
Joakim Sandström <joakim.s@webbkontakt.net>
2016-12-29 18:45:38 +0100

    Add files via upload

--- install_x11.sh
+++ install_x11.sh
@@ -4,15 +4,15 @@ dialog --title "Installing X11 and LXDE-core" --infobox "\nThise will take some 
 sudo apt-get -y install lxde-core xserver-xorg xinit fbi
 dialog --title "Download and install" --infobox "\nDownloading chromium and installing\n"  11 70
 wget -q http://ftp.us.debian.org/debian/pool/main/libg/libgcrypt11/libgcrypt11_1.5.0-5+deb7u4_armhf.deb
-wget -q https://bugs.launchpad.net/~canonical-chromium-builds/+archive/ubuntu/stage/+build/9739240/+files/chromium-browser_50.0.2661.102-0ubuntu0.14.04.1.1117_armhf.deb
-wget -q https://bugs.launchpad.net/~canonical-chromium-builds/+archive/ubuntu/stage/+build/9739240/+files/chromium-codecs-ffmpeg-extra_50.0.2661.102-0ubuntu0.14.04.1.1117_armhf.deb
+wget -q http://dl.bintray.com/kusti8/chromium-rpi/pool/main/c/chromium-codecs-ffmpeg-extra/chromium-codecs-ffmpeg-extra_51.0.2704.79-0ubuntu0.14.04.1.1121_armhf.deb
+wget -q http://dl.bintray.com/kusti8/chromium-rpi/pool/main/c/chromium-browser/chromium-browser_51.0.2704.79-0ubuntu0.14.04.1.1121_armhf.deb
 sudo dpkg -i libgcrypt11_1.5.0-5+deb7u4_armhf.deb 2>&1
-sudo dpkg -i chromium-codecs-ffmpeg-extra_50.0.2661.102-0ubuntu0.14.04.1.1117_armhf.deb 2>&1
-sudo dpkg -i chromium-browser_50.0.2661.102-0ubuntu0.14.04.1.1117_armhf.deb 2>&1
+sudo dpkg -i chromium-codecs-ffmpeg-extra_51.0.2704.79-0ubuntu0.14.04.1.1121_armhf.deb 2>&1
+sudo dpkg -i chromium-browser_51.0.2704.79-0ubuntu0.14.04.1.1121_armhf.deb 2>&1
 if [ $? -gt 0 ]; then
 dialog --title "Installing dependencies..." --infobox "\nPlease wait...\n" 11 70
 sudo apt-get -f --force-yes --yes install >/dev/null 2>&1
-sudo dpkg -i chromium-browser_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb 2>&1
+sudo dpkg -i chromium-browser_51.0.2704.79-0ubuntu0.14.04.1.1121_armhf.deb 2>&1
 fi
 dialog --title "Download and install" --infobox "\nDownloading launcher and installing\n"  11 70
 wget -q https://github.com/zjoasan/x11-osmc/raw/master/install_x11.zip 2>&1
@@ -25,4 +25,3 @@ sudo chmod 777 /etc/alternatives/desktop-background 2>&1
 xbmc-send -a "UpdateLocalAddons" >/dev/null
 dialog --title "Installation finnished!" --msgbox "\nThank you for using my installer\n"  11 70
 exit
-
