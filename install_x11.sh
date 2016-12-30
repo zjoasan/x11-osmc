c19732a95fb5a7cc87ea75f94db764cfd7fd0634
Joakim Sandström <joakim.s@webbkontakt.net>
2016-12-30 16:52:41 +0100

    Update install_x11.sh

--- install_x11.sh
+++ install_x11.sh
@@ -14,14 +14,14 @@ dialog --title "Installing dependencies..." --infobox "\nPlease wait...\n" 11 70
 sudo apt-get -f --force-yes --yes install >/dev/null 2>&1
 sudo dpkg -i chromium-browser_51.0.2704.79-0ubuntu0.14.04.1.1121_armhf.deb 2>&1
 fi
-dialog --title "Download and install" --infobox "\nDownloading launcher and installing\n"  11 70
+dialog --title "Download and install" --infobox "\nDownloading launcher and installing\n" 11 70
 wget -q https://github.com/zjoasan/x11-osmc/raw/master/install_x11.zip 2>&1
 unzip -q -o install_x11.zip 2>&1
 chmod +x /home/osmc/x11-start/x_init.sh 2>&1
 chmod +x /home/osmc/x11-start/xstart.sh 2>&1
-dialog --title "Post setup tweaks" --infobox "\nChangeing default background in X11 and updating Kodi addons \nto reflect X11 launcher"  11 70
+dialog --title "Post setup tweaks" --infobox "\nChangeing default background in X11 and updating Kodi addons \nto reflect X11 launcher" 11 70
 sudo ln /usr/splash.png /etc/alternatives/desktop-background 2>&1
 sudo chmod 777 /etc/alternatives/desktop-background 2>&1
 xbmc-send -a "UpdateLocalAddons" >/dev/null
-dialog --title "Installation finnished!" --msgbox "\nThank you for using my installer\n"  11 70
+dialog --title "Installation finnished!" --msgbox "\nThank you for using my installer\n" 11 70
 exit
