afc52d8ee1a79ea9269936f2369253548872b184
Joakim Sandström <joakim.s@webbkontakt.net>
2019-08-19 15:36:53 +0200

    forgot chmod
    Forgot to add the  the script files chmod

--- install_x11.sh
+++ install_x11.sh
@@ -28,6 +28,7 @@ wget -q https://github.com/zjoasan/x11-osmc/raw/master/install_x11.zip 2>&1
 unzip -q -o install_x11.zip 2>&1
 chmod +x /home/osmc/x11-start/x_init.sh 2>&1
 chmod +x /home/osmc/x11-start/xstart.sh 2>&1
+chmod +x /home/osmc/x11-start/virtualkeyboard-toogle.sh 2>&1
 dialog --title "Post setup tweaks" --infobox "\nChangeing default background in X11 and updating Kodi addons \nto reflect X11 launcher" 11 70
 sudo rm /usr/share/applications/kodi.desktop
 sudo rm /usr/share/xsessions/kodi.desktop
