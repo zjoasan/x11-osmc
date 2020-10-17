0291e9492fcb68fd959767bf2cb4cb0bbac5277f
Joakim Sandström <joakim.s@webbkontakt.net>
2020-10-17 20:40:27 +0200

    Update install_x11.sh
    Added language, lang, lc_all and ncurses for line exports, quick and dirty buster detection to deside which chromium package to use, check if mediacenter is running before trying to updatelocaladdon and check if lightdm is running before trying to close it.

--- install_x11.sh
+++ install_x11.sh
@@ -6,21 +6,33 @@
                 exit 1
         fi
 
+#Set UTF-8; e.g. "en_US.UTF-8" or "de_DE.UTF-8":
+export LANGUAGE="en_US.UTF-8"
+export LANG="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
+sudo locale-gen en_US.UTF-8
+
+#Tell ncurses to use line characters that work with UTF-8.
+export NCURSES_NO_UTF8_ACS=1
 
 sudo apt-get update 2>&1 | dialog --title "Updating package database..." --infobox "\nPlease wait...\n" 11 70
 
 dialog --title "Installing X11, LXDE-core and Chromium" --infobox "\nThise will take some time so please wait...\n" 11 70
 
 sudo apt-get -y install lxde-core xserver-xorg xinit fbi libinput-dev xwit xdotool x11-utils xvkbd zenity xterm
-wget http://launchpadlibrarian.net/380369885/chromium-codecs-ffmpeg-extra_68.0.3440.75-0ubuntu0.16.04.1_armhf.deb
-wget http://launchpadlibrarian.net/380369879/chromium-browser_68.0.3440.75-0ubuntu0.16.04.1_armhf.deb
-sudo dpkg -i chromium-codecs-ffmpeg-extra_68.0.3440.75-0ubuntu0.16.04.1_armhf.deb
-sudo dpkg -i chromium-browser_68.0.3440.75-0ubuntu0.16.04.1_armhf.deb
-sudo apt --fix-broken install
-sudo dpkg -i chromium-browser_68.0.3440.75-0ubuntu0.16.04.1_armhf.deb
-if [ $? -gt 0 ]; then
-dialog --title "Installing dependencies..." --infobox "\nPlease wait...\n" 11 70
-sudo apt-get -f --force-yes --yes install >/dev/null 2>&1
+if (( $(lsb_release -d | grep -v grep | grep "buster" | wc -l) > 0 )); nthen
+       sudo apt-get install chromium-common chromium
+else
+       wget http://launchpadlibrarian.net/380369885/chromium-codecs-ffmpeg-extra_68.0.3440.75-0ubuntu0.16.04.1_armhf.deb
+       wget http://launchpadlibrarian.net/380369879/chromium-browser_68.0.3440.75-0ubuntu0.16.04.1_armhf.deb
+       sudo dpkg -i chromium-codecs-ffmpeg-extra_68.0.3440.75-0ubuntu0.16.04.1_armhf.deb
+       sudo dpkg -i chromium-browser_68.0.3440.75-0ubuntu0.16.04.1_armhf.deb
+       sudo apt --fix-broken install
+       sudo dpkg -i chromium-browser_68.0.3440.75-0ubuntu0.16.04.1_armhf.deb
+       if [ $? -gt 0 ]; then
+              dialog --title "Installing dependencies..." --infobox "\nPlease wait...\n" 11 70
+              sudo apt-get -f --force-yes --yes install >/dev/null 2>&1
+       fi
 fi
 dialog --title "Download and install" --infobox "\nDownloading launcher and installing\n" 11 70
 
@@ -36,9 +48,14 @@ sudo rm /usr/share/xsessions/kodi.desktop
 
 sudo ln /usr/splash.png /etc/alternatives/desktop-background 2>&1
 sudo chmod 777 /etc/alternatives/desktop-background 2>&1
-xbmc-send -a "UpdateLocalAddons"
+
+if (( $(ps -ef | grep -v grep | grep "mediacenter" | wc -l) > 0 )); then
+       xbmc-send -a "UpdateLocalAddons"
+fi
 
 dialog --title "Installation finished!" --msgbox "\nThank you for using my installer\n" 11 70
-sudo systemctl disable lightdm
+if (( $(ps -ef | grep -v grep | grep "lightdm" | wc -l) > 0 )); nthen
+       sudo systemctl disable lightdm
+fi
 exit
 
