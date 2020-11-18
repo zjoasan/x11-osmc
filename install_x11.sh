dc3c8256608e41f48749266b6c9987d5b6ba9ec6
Joakim Sandström <joakim.s@webbkontakt.net>
2020-11-18 15:06:02 +0100

    Update install_x11.sh
    Redirected sterr & stout to dev/null for locale-gen command

--- install_x11.sh
+++ install_x11.sh
@@ -10,7 +10,7 @@
 export LANGUAGE="en_US.UTF-8"
 export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
-sudo locale-gen en_US.UTF-8
+sudo locale-gen en_US.UTF-8 &> /dev/null
 
 #Tell ncurses to use line characters that work with UTF-8.
 export NCURSES_NO_UTF8_ACS=1
