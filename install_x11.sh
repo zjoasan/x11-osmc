6a854a8705018e13fe86972c9aee7439b5987ac6
Joakim Sandström <joakim.s@webbkontakt.net>
2020-11-19 20:09:16 +0100

    Update install_x11.sh
    bash spell checker fixews

--- install_x11.sh
+++ install_x11.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-       if [ "$(id -u)" != "1000" ]; then
-                echo
-                echo "This script must be run as osmc user." 1>&2
-                echo
-                exit 1
-        fi
-
+if [ "$(id -u)" != "1000" ]; then
+       echo
+       echo "This script must be run as osmc user." 1>&2
+       echo
+       exit 1
+fi
 #Set UTF-8; e.g. "en_US.UTF-8" or "de_DE.UTF-8":
 export LANGUAGE="en_US.UTF-8"
 export LANG="en_US.UTF-8"
@@ -20,7 +19,7 @@ sudo apt-get update 2>&1 | dialog --title "Updating package database..." --infob
 dialog --title "Installing X11, LXDE-core and Chromium" --infobox "\nThise will take some time so please wait...\n" 11 70
 
 sudo apt-get -y install lxde-core xserver-xorg xinit fbi libinput-dev xwit xdotool x11-utils xvkbd zenity xterm
-if (( $(lsb_release -d | grep -v grep | grep "buster" | wc -l) > 0 )); then
+if (( $(lsb_release -d | grep -v grep | grep -c "buster") > 0 )); then
        sudo apt-get install chromium-common chromium
 else
        wget http://launchpadlibrarian.net/380369885/chromium-codecs-ffmpeg-extra_68.0.3440.75-0ubuntu0.16.04.1_armhf.deb
@@ -49,19 +48,14 @@ sudo rm /usr/share/xsessions/kodi.desktop
 sudo ln /usr/splash.png /etc/alternatives/desktop-background 2>&1
 sudo chmod 777 /etc/alternatives/desktop-background 2>&1
 
-if (( $(ps -ef | grep -v grep | grep "mediacenter" | wc -l) > 0 )); then
+if (( $(pgrep -c "mediacenter") > 0 )); then
        xbmc-send -a "UpdateLocalAddons"
 fi
 
 dialog --title "Installation finished!" --msgbox "\nThank you for using my installer\n" 11 70
-<<<<<<< HEAD
-sleep 5
-if (( $(ps -ef | grep -v grep | grep "lightdm" | wc -l) > 0 )); then
-=======
-if (( $(dpkg -s lightdm | grep ‘Status: install ok installed’| wc -l) > 0 )); then
+
+if (( $(dpkg -s lightdm | grep -c "Status: install ok installed") > 0 )); then
        sudo systemctl stop lightdm
->>>>>>> dc3c8256608e41f48749266b6c9987d5b6ba9ec6
        sudo systemctl disable lightdm
 fi
 exit
-
