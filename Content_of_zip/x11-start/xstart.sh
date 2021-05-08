c889b183e756a5752157f88f4b2c015044e106b6
Joakim Sandström <joakim.s@webbkontakt.net>
2021-05-08 14:00:17 +0200

    Delete Content_of_zip directory

--- Content_of_zip/x11-start/xstart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo openvt -c 7 -s -f clear
-sudo su osmc -c "openvt -c 7 -f -s /home/osmc/x11-start/x_init.sh &" &
-sleep 1
-sudo su -c "xbmc-send -a "Quit" | sudo systemctl stop mediacenter" &
-exit
-
-
